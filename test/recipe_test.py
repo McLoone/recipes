6e47e7582c1c61943cf1382b58d1d2ecde34c23e
P-A Söderqvist <pa.soderqvist@gmail.com>
2016-01-31 19:05:54 +0100

    Refactoring unittests

--- test/recipe_test.py
+++ test/recipe_test.py
@@ -3,39 +3,37 @@ from recipe import RecipeStore, Recipe
 
 
 class RecipeTest(unittest.TestCase):
+    def setUp(self):
+        self.recipe_store = RecipeStore()
+
     def test_add_recipe(self):
-        recipe_store = RecipeStore()
         recipe = Recipe("description")
 
-        recipe.id = recipe_store.add_recipe(recipe)
+        recipe.id = self.recipe_store.add_recipe(recipe)
 
-        self.assertListEqual([recipe], recipe_store.list_recipes())
+        self.assertListEqual([recipe], self.recipe_store.list_recipes())
 
     def test_add_multiple_recipies(self):
-        recipe_store = RecipeStore()
         recipe1 = Recipe("description1")
         recipe2 = Recipe("description2")
         recipe3 = Recipe("description3")
-        recipe1.id = recipe_store.add_recipe(recipe1)
-        recipe2.id = recipe_store.add_recipe(recipe2)
-        recipe3.id = recipe_store.add_recipe(recipe3)
+        recipe1.id = self.recipe_store.add_recipe(recipe1)
+        recipe2.id = self.recipe_store.add_recipe(recipe2)
+        recipe3.id = self.recipe_store.add_recipe(recipe3)
 
-        return_list = recipe_store.list_recipes()
+        return_list = self.recipe_store.list_recipes()
         return_list.sort(key=lambda r: r.description)
 
         self.assertListEqual([recipe1, recipe2, recipe3], return_list)
 
     def test_fetch_from_empty_store(self):
-        recipe_store = RecipeStore()
-
-        self.assertListEqual([], recipe_store.list_recipes())
+        self.assertListEqual([], self.recipe_store.list_recipes())
 
     def test_delete_recipe(self):
-        recipe_store = RecipeStore()
         recipe = Recipe("description")
 
-        recipe.id = recipe_store.add_recipe(recipe)
-        self.assertListEqual([recipe], recipe_store.list_recipes())
+        recipe.id = self.recipe_store.add_recipe(recipe)
+        self.assertListEqual([recipe], self.recipe_store.list_recipes())
 
-        removed_recipe = recipe_store.remove_recipe(recipe.id)
-        self.assertListEqual([], recipe_store.list_recipes())
+        removed_recipe = self.recipe_store.remove_recipe(recipe.id)
+        self.assertListEqual([], self.recipe_store.list_recipes())
